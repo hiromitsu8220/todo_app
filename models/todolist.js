4d01e11a3c79a25ef5cc2b33ad5eedab877a3cbd
駒井宏光 <hiro.koma.ghq@softbank.ne.jp>
2018-07-07 02:24:51 +0900

    add todolist model and controller

---
+++ models/todolist.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const todolistSchema = new Schema({
+  item: { type: String, required: true, max: 100 },
+  content: { type: String },
+  add_date: { type: Date },
+  limit_date: { type: Date }
+});
+
+
+todolistSchema.static('findByItem', function (item, callback) {
+  return this.find({ item: item }, callback);
+});
+
+//Export model
+module.exports = mongoose.model('ToDoList', todolistSchema);
